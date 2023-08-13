5da7af7220cad60b087eb54898e33e854da993bc
acer <acer@ASUR>
2023-08-13 21:26:52 +0530

    Add project files.

---
+++ LargestEle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestEle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 25, 5, 30, 15 };
+            int largest = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                if (num > largest)
+                {
+                    largest = num;
+                }
+            }
+
+            Console.WriteLine("Largest element: " + largest);
+            Console.ReadLine();
+        }
+    }
+}
